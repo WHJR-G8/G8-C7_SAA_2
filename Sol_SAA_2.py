4b9ba6ee46d4a5ad41949bb787070050bd802291
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-28 10:53:02 +0530

    Create Sol_SAA_2.py

---
+++ Sol_SAA_2.py
@@ -0,0 +1,12 @@
+def operations():
+    number1 = int(input("Enter the number 1 : "))
+    number2 = int(input("Enter the number 2 : "))
+    print("The addition is : ", number1+number2)
+    print("The subtraction is : ", number1-number2)
+    print("The multiplication is : ", number1*number2)
+    print("The division is : ", number1/number2)
+
+operations()
+operations()
+operations()
+operations()
